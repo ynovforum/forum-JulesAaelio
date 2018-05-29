5d5c8f0f25842d989200659fed33631d58a67d94
Jules LAURENT <jules.laurent@ynov.com>
2018-05-29 08:59:59 +0200

    externalize auth guard 2

--- src/utils/auth-guard.js
+++ src/utils/auth-guard.js
@@ -1,6 +1,5 @@
 module.exports = (req,res,next) => {
     if(req.user) {
-        res.locals.user = req.user;
         next();
     }else if (req.originalUrl.match('/login') === null && req.originalUrl.match('/register') === null){
         res.redirect('/login?redirectTo='+req.originalUrl);
--- src/utils/default.js
+++ src/utils/default.js
@@ -1,4 +1,11 @@
 module.exports = (app) => {
+    app.use((req,res,next) => {
+        if(req.user) {
+            res.locals.user = req.user;
+        }
+        next();
+    });
+
     app.get('/',(req,res) => {
         res.render('base');
     });
