55ad72f589750c27b095075d5b9107d4da5425fb
Jules LAURENT <jules.laurent@ynov.com>
2018-05-30 10:27:15 +0200

    wip edition toggle for the description

--- src/assets/scripts/question-edit.js
+++ src/assets/scripts/question-edit.js
@@ -1,1 +1,5 @@
-alert('test2ccccddedecd');
+$(".edit-question").click((r) => {
+    let description = $('.question-description');
+    description.replaceWith('<textarea value="'+ description.innerHTML + '">' +
+        '</textarea>' );
+});
