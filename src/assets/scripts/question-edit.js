3edcb22d1c6d89d155626dfa1a7cb216f6d38896
Jules LAURENT <jules.laurent@ynov.com>
2018-05-30 11:46:26 +0200

    question is editable locally

--- src/assets/scripts/question-edit.js
+++ src/assets/scripts/question-edit.js
@@ -1,5 +1,27 @@
-$(".edit-question").click((r) => {
-    let description = $('.question-description');
-    description.replaceWith('<textarea value="'+ description.innerHTML + '">' +
-        '</textarea>' );
+let description = $('.question-description');
+let title = $('.question-title');
+let textArea = $('<textarea id="description">' + '</textarea>');
+
+$(".edit-question").click((e) => {
+    if(e.target.classList.contains('fa-pencil-alt')) {
+        let descriptionContent = description[0].innerHTML;
+        description.hide();
+        // description.replaceWith(textArea);
+        description.parent().append(textArea);
+        textArea.val(descriptionContent);
+        tinymce.init({
+            selector : '#description'
+        });
+
+        title.removeAttr('disabled');
+        $(".edit-question").toggleClass('fa-pencil-alt').toggleClass('fa-save');
+    } else {
+        title.attr('disabled','disabled');
+        description.html(tinyMCE.activeEditor.getContent());
+        description.show();
+        tinyMCE.activeEditor.remove();
+        textArea.remove();
+        $(".edit-question").toggleClass('fa-pencil-alt').toggleClass('fa-save');
+    }
+
 });
