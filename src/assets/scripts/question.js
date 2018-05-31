d78563eadbe597470e2a6bf19281e2c1c501909f
Jules LAURENT <jules.laurent@ynov.com>
2018-05-31 12:38:51 +0200

    question templating

--- src/assets/scripts/question.js
+++ src/assets/scripts/question.js
@@ -1,3 +1,5 @@
 tinymce.init({
-    selector: '#description-field'
+    selector: '#description-field',
+    menubar:false,
+    toolbar: 'bold italic underline | alignleft alignright aligncenter alignjustify | blockquote'
 });
